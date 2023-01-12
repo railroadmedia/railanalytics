22db2d415cabfc18ece6c2a4e200074a30d53dbf
calebfavor <calebfavor@gmail.com>
2023-01-12 14:46:55 -0800

    tesitng...

--- src/Controllers/BlankTrackingPageController.php
+++ src/Controllers/BlankTrackingPageController.php
@@ -20,11 +20,21 @@ class BlankTrackingPageController
             Log::info('BlankTrackingPageController show $cacheKey: ' . $cacheKey);
 
             $cacheTrackingData = Cache::store('redis')->get($cacheKey);
+            $store = Cache::store('redis');
+
+            Log::info('Cache Prefix railanalytics: ' . $this->getProtectedValue($this->getProtectedValue(Cache::store('redis'), 'store'), 'prefix'));
+
             Log::info('BlankTrackingPageController show $cacheTrackingData: ' . var_export($cacheTrackingData, true));
 
 //            Cache::store('redis')->forget($cacheKey);
         }
 
         return view('railanalytics::blank-tracking-page', ['cacheKey' => $cacheKey, 'cacheTrackingData' => $cacheTrackingData]);
+    }
+
+    public function getProtectedValue($obj, $name) {
+        $array = (array)$obj;
+        $prefix = chr(0).'*'.chr(0);
+        return $array[$prefix.$name];
     }
 }
