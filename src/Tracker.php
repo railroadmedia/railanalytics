1f0c3e89b37627da88e3de451f00e5dc824faaf0
adrian-musora <adrian@musora.com>
2024-03-14 13:38:19 -0700

    Remove js tags from tracking provider

--- src/Tracker.php
+++ src/Tracker.php
@@ -27,7 +27,7 @@ use Railroad\Railanalytics\TrackingProviders\TrackingProviderFactory;
  * @method static string trackTransaction(array $products, $transactionId, $revenue, $tax, $shipping, $paymentType, $promoCode, $currency = 'USD')
  * @method static string trackLead($value = null, $currency = 'USD')
  * @method static string trackTransactionAPI(array $products, $transactionId, $promoCode, $userID, $email, $currency = 'USD', $affiliateClickCode=null)
-
+ * @method static string trackEverFlowConversionAPI($orderID, $email, $timestamp)
  */
 class Tracker
 {
